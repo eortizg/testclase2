9226eff0b8159e4ed69178737432ce430b66ac72
Erick Ortiz <eortiz@canvia.com>
2021-03-08 10:42:35 -0500

    .

--- infra.tf
+++ infra.tf
@@ -31,20 +31,20 @@ resource "azurerm_storage_account" "example" {
 #cosmodb
 data "azurerm_cosmosdb_account" "example" {
   name                = "tfex-cosmosdb-account"
-  resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
+  resource_group_name = data.azurerm_resource_group.example.resource_group_name
 }
 
 resource "azurerm_cosmosdb_mongo_database" "example" {
   name                = "tfex-cosmos-mongo-db"
-  resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
+  resource_group_name = data.azurerm_resource_group.example.resource_group_name
   account_name        = data.azurerm_cosmosdb_account.example.name
 }
 
 resource "azurerm_cosmosdb_mongo_collection" "example" {
   name                = "tfex-cosmos-mongo-db"
-  resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
+  resource_group_name = data.azurerm_resource_group.example.resource_group_name
   account_name        = data.azurerm_cosmosdb_account.example.name
-  database_name       = azurerm_cosmosdb_mongo_database.example.name
+  database_name       = data.azurerm_cosmosdb_mongo_database.example.name
 
   default_ttl_seconds = "777"
   shard_key           = "uniqueKey"
