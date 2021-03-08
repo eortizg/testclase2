596aa72719902d9e8a097a5205d550528479f06b
Erick Ortiz <eortiz@canvia.com>
2021-03-08 10:56:20 -0500

    .

--- infra.tf
+++ infra.tf
@@ -22,3 +22,21 @@ data "azurerm_cosmosdb_account" "example" {
   name                = "tfex-cosmosdb-account"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 }
+
+resource "azurerm_cosmosdb_mongo_database" "example" {
+  name                = "tfex-cosmos-mongo-db"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  account_name        = data.azurerm_cosmosdb_account.example.name
+}
+
+resource "azurerm_cosmosdb_mongo_collection" "example" {
+  name                = "tfex-cosmos-mongo-db"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  account_name        = data.azurerm_cosmosdb_account.example.name
+  database_name       = data.azurerm_cosmosdb_mongo_database.example.name
+
+  default_ttl_seconds = "777"
+  shard_key           = "uniqueKey"
+  throughput          = 400
+}
+
